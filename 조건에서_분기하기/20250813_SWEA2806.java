ec0262e95594118b094dbfbfb84a655029b33f63
sabizupa <76557564+sabizupa@users.noreply.github.com>
2025-09-05 15:06:47 +0900

    SWEA2806. N-Queen
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GKs06AU0DFAXB

---
+++ 조건에서_분기하기/20250813_SWEA2806.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 20250813_SWEA2806 {
+	static int N;
+	static int[] candidates;
+	static int result;
+    // 참고함: https://wikidocs.net/206359
+	public static void calc(int row) {
+		if (row == N) {
+			result++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			candidates[row] = i;
+			if (isAvailable(row)) {
+				calc(row + 1);
+			}
+		}
+	}
+
+	public static boolean isAvailable(int row) {
+		for (int i = 0; i < row; i++) {
+			if (candidates[row] == candidates[i] ||Math.abs(candidates[row] - candidates[i]) ==  row - i) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc=1; tc<=T; tc++) {
+			result = 0;
+			N = sc.nextInt();
+			candidates = new int[N];
+			calc(0);
+			System.out.println("#"+tc+" "+result);
+		}
+
+	}
+
+}
